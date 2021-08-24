3be571c3ba5e5f9b02fa3edf382a6290ae55dd7e
solanamr <romerosolana@gmail.com>
2021-08-24 12:02:48 -0300

    adding files for landing page

--- README.md
+++
@@ -1,2 +0,0 @@
-# gitPractice
-This is a repo for you to  learn git, github, gitflow. Let's have some fun!
--- ejercicio.js
+++
@@ -1,8 +0,0 @@
-//Tenemos dos objetos, uno con la información de usuario, y otro con la información personal de dicho usuario, queremos obtener
-//un solo objeto con toda la información.
-
-const usuario1 = { id: "123", username: "pepe24" }
-const usuario1perfil = { nombre: "pepe", edad: 27, profesion: "programador" }
-
-const resultado = {...usuario1,...usuario1perfil}
-console.log(resultado)
