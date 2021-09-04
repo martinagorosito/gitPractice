35ec2ae2f566c379564f784c88372403178a576e
FlorenciaPD <florperezduarte@hotmail.com.ar>
2021-09-04 15:34:04 -0300

    update: added contact form to conocenos page

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ for (let i=0 ; i< pictures.length ; i++){
     })
     
 }
+
+const button = document.querySelector(".button")
+button.addEventListener('click', () =>{
+    button.style.background= "#E5E5E5";
+    button.style.color= "rgba(235, 87, 87, 1)";
+    button.style.border= "3px solid rgba(235, 87, 87, 1)";
+})
+
+const buttonContact= document.querySelector (".form-container .button")
+buttonContact.addEventListener('click', ()=>{
+    buttonContact.style.background="rgba(255, 255, 255, 1)";
+
+})
